c20a5d64b1d1beb7f09f64d51d40031bdfca9d2f
YashMandlekar <mandlekarvansh@gmail.com>
2026-04-20 09:38:35 +0530

    debug3

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,5 +4,14 @@ const API = axios.create({
   baseURL: "https://fswdecommerce.onrender.com/api",
 });
 
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return req;
+});
 
 export default API;
